428ac957b84fc28d2354a75b80822bcc543687d2
vinhha <vinhha@vng.com.vn>
2018-04-03 14:37:50 +0700

    Add button to open ZaloPay App

--- app/src/main/java/vn/com/zalopay/demozpdk/MainActivity.java
+++ app/src/main/java/vn/com/zalopay/demozpdk/MainActivity.java
@@ -3,17 +3,27 @@ package vn.com.zalopay.demozpdk;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import vn.com.zalopay.demozpdk.ZPDKListener.MerchantListener;
 import vn.zalopay.sdk.ZaloPaySDK;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mButtonPayOrder ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ZaloPaySDK.getInstance().payOrder(this, "ZPTransToken", new MerchantListener(this));
+        mButtonPayOrder = (Button) findViewById(R.id.btn_payorder);
+        mButtonPayOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ZaloPaySDK.getInstance().payOrder(MainActivity.this, "ZPTransToken", new MerchantListener(MainActivity.this));
+            }
+        });
     }
 
     @Override
