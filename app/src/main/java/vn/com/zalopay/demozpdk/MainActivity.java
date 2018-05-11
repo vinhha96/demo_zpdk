0255b2a54dbdd0f60ba7fc4a7e08e22f0cf910d8
vinhha <vinhha@vng.com.vn>
2018-05-12 03:07:11 +0700

    [App] Add library to scan QR code to pay order
    + Improve UI
    + Refactor code

--- app/src/main/java/vn/com/zalopay/demozpdk/MainActivity.java
+++ app/src/main/java/vn/com/zalopay/demozpdk/MainActivity.java
@@ -1,35 +1,60 @@
 package vn.com.zalopay.demozpdk;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
-import vn.com.zalopay.demozpdk.ZPDKListener.MerchantListener;
-import vn.zalopay.sdk.ZaloPaySDK;
+import vn.com.zalopay.demozpdk.Scan.ScanActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mButtonPayOrder ;
+    Button mButtonScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        transparentToolbar();
         setContentView(R.layout.activity_main);
-        mButtonPayOrder = (Button) findViewById(R.id.btn_payorder);
-        mButtonPayOrder.setOnClickListener(new View.OnClickListener() {
+
+        mButtonScan = findViewById(R.id.btn_scan);
+
+        mButtonScan.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // @param "ZPTransToken" is virtual value to test. Merchant must request server ZaloPay to get data correctly
-                ZaloPaySDK.getInstance().payOrder(MainActivity.this, "rghBdODENaIh9NRACLMnaw", new MerchantListener(MainActivity.this));
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ScanActivity.class));
             }
         });
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        ZaloPaySDK.getInstance().onActivityResult(requestCode, resultCode, data);
+    private void transparentToolbar() {
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 }
--- app/src/main/java/vn/com/zalopay/demozpdk/ZPDKListener/MerchantListener.java
+++ app/src/main/java/vn/com/zalopay/demozpdk/ZPDKListener/MerchantListener.java
@@ -1,20 +1,22 @@
 package vn.com.zalopay.demozpdk.ZPDKListener;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 
+import vn.com.zalopay.demozpdk.Constant.ConstantCode;
 import vn.com.zalopay.demozpdk.Constant.PaymentErrorCode;
-import vn.com.zalopay.demozpdk.MainActivity;
+import vn.com.zalopay.demozpdk.Helper.Dialog;
 import vn.zalopay.sdk.ZaloPayErrorCode;
 import vn.zalopay.sdk.ZaloPayListener;
 import vn.zalopay.sdk.ZaloPaySDK;
 
+
 public class MerchantListener implements ZaloPayListener {
-    private String TAG = "Merchant Callback";
     private Context mContext;
 
     public MerchantListener(Context mContext) {
@@ -22,18 +24,15 @@ public class MerchantListener implements ZaloPayListener {
     }
 
     @Override
-    public void onPaymentSucceeded(String transactionId) {
+    public void onPaymentSucceeded(String transactionId, String transToken) {
         @SuppressLint("DefaultLocale")
-        String mes = String.format("onPaymentSuccess: On successful with transactionId: %s]", transactionId);
-        new AlertDialog.Builder(mContext)
-                .setTitle("Payment Success")
-                .setMessage(mes)
-                .setNegativeButton("Yes", null).show();
-        Log.d(TAG, "onSuccess: On successful with transactionId:" + transactionId);
+        Spanned mes = Html.fromHtml(String.format("onPaymentSuccess: <br><br> On successful with: <br>- transactionId: <b> %s  </b> <br> - transtoken: <b>  %s </b> ", transactionId, transToken));
+        Dialog.showNaturalDialog(mContext, "Payment Success", mes, "Yes");
+        Log.d(ConstantCode.TAG, "onSuccess: On successful with transactionId: " + transactionId + "- transtoken: %s" + transToken);
     }
 
     @Override
-    public void onPaymentError(ZaloPayErrorCode zaloPayErrorCode, int paymentErrorCode) {
+    public void onPaymentError(ZaloPayErrorCode zaloPayErrorCode, int paymentErrorCode, String zpTransToken) {
         if (zaloPayErrorCode == ZaloPayErrorCode.ZALO_PAY_NOT_INSTALLED) {
             new AlertDialog.Builder(mContext)
                     .setTitle("Error Payment")
@@ -45,23 +44,17 @@ public class MerchantListener implements ZaloPayListener {
                         }
                     })
                     .setNegativeButton("Back", null).show();
-            Log.d(TAG, "onError: ZaloPay App not install on this Device.");
+            Log.d(ConstantCode.TAG, "onError: <br> <b> <i> ZaloPay App not install on this Device. </i> </b>");
         } else if (zaloPayErrorCode == ZaloPayErrorCode.PAYMENT_ERROR && paymentErrorCode == PaymentErrorCode.USER_CANCEL.getValue()) {
             @SuppressLint("DefaultLocale")
-            String mes = "Payment error: User click back return to app.";
-            new AlertDialog.Builder(mContext)
-                    .setTitle("Error Payment")
-                    .setMessage(mes)
-                    .setNegativeButton("Yes", null).show();
-            Log.d(TAG, mes);
+            Spanned mes = Html.fromHtml("<b> Payment error <b> : User click back return to app.");
+            Dialog.showNaturalDialog(mContext, "Payment Error", mes, "Yes");
+            Log.d(ConstantCode.TAG, String.valueOf(mes));
         } else {
             @SuppressLint("DefaultLocale")
-            String mes = String.format("onPaymentError: payment error with [error: %s, paymentErrorCode: %d]", zaloPayErrorCode, paymentErrorCode);
-            new AlertDialog.Builder(mContext)
-                    .setTitle("Error Payment")
-                    .setMessage(mes)
-                    .setNegativeButton("Yes", null).show();
-            Log.d(TAG, mes);
+            Spanned mes = Html.fromHtml(String.format("onPaymentError: <br> <br> payment error with: <br> - error: <b> %s </b> <br> - paymentErrorCode: <b> %d </b> <br> - zpTransToken: <b> %s </b>", zaloPayErrorCode, paymentErrorCode, zpTransToken));
+            Dialog.showNaturalDialog(mContext, "Payment Error", mes, "Yes");
+            Log.d(ConstantCode.TAG, String.valueOf(mes));
         }
     }
 }
